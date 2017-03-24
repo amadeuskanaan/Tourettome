94f9ce77084157e5d510e1e080fa676c3599c83c
kanaan <amadeus.kanaan@gmail.com>
2017-03-24 16:40:01 +0100

    diff

---
+++ print_pop_lengths.py
@@ -0,0 +1,7 @@
+from variables.subject_list import *
+
+print 'PARIS len =', len(paris)
+print 'Leipzig len =', len(leipzig)
+print 'Hannover_a len =', len(hannover_a)
+print 'Hannover_b len =', len(hannover_b)
+print 'Tourettome subjects =', len(tourettome_subjects)
