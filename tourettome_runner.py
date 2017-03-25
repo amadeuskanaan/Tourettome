cb5f4e6c4e10c3ccd27c2fd8b95339dab9fae70a
amadeuskanaan <amadeus.kanan@gmail.com>
2017-03-25 23:22:41 +0100

    push

--- tourettome_runner.py
+++ tourettome_runner.py
@@ -3,7 +3,7 @@ __author__ = 'kanaan.. 25.03.2017'
 import os
 
 
-os.system('python 000_data_data.py      && '
+os.system('python 000_get_data.py       && '
           'python 001_preproc_anat.py   && '
           'python 003_kelly_kapowski.py && '
           'python 004_preproc_func.py   && '
