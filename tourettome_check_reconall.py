70fdd03e7e66bf29b2777a460cd6749de8ecb742
amadeuskanaan <amadeus.kanan@gmail.com>
2017-03-25 23:20:22 +0100

    push

--- tourettome_check_reconall.py
+++ tourettome_check_reconall.py
@@ -8,6 +8,7 @@ print '                  Tourettome  Population                        '
 
 
 
+# print sample sizes
 print 'Tourettome - Leipzig      N=', len(leipzig)
 print 'Tourettome - Paris        N=', len(paris)
 print 'Tourettome - Hannover_a   N=', len(hannover_a)
@@ -15,15 +16,11 @@ print 'Tourettome - Hannover_b   N=', len(hannover_b)
 
 
 
+# check recon-all outputs
 fsdir= tourettome_freesurfer
-population = [i for i in sorted(os.listdir(fsdir)) if os.path.isdir(os.path.join(fsdir, i))]
+population= [i for i in sorted(os.listdir(fsdir)) if os.path.isdir(os.path.join(fsdir, i))]
+missing= [sub for sub in population if 'aparc.DKTatlas+aseg.mgz' not in os.listdir(os.path.join(fsdir, sub, 'mri'))]
 
-missing = []
-
-for subject in population:
-	subdir = os.path.join(fsdir, subject, 'mri')
-	if 'aparc.DKTatlas+aseg.mgz' not in os.listdir(subdir):
-		missing.append(subject)
-
+print ''
 print 'These subjects have an incomplete recon-all directory. Run them again.',  missing
 print 'N=', len(missing)
