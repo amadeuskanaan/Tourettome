f2ecf1528febba7077d5834735a8631942794c66
amadeuskanaan <amadeus.kanan@gmail.com>
2017-03-25 23:22:13 +0100

    push

--- tourettome_runner.py
+++ tourettome_runner.py
@@ -7,5 +7,5 @@ os.system('python 000_data_data.py      && '
           'python 001_preproc_anat.py   && '
           'python 003_kelly_kapowski.py && '
           'python 004_preproc_func.py   && '
-          'python 005_transform.py      && ')
+          'python 005_transform.py         ')
 
