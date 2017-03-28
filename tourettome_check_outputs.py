8a0c6203c9fc1cf3033eaa40f8f886527905599c
amadeuskanaan <amadeus.kanan@gmail.com>
2017-03-28 20:18:44 +0200

    push

--- tourettome_check_outputs.py
+++ tourettome_check_outputs.py
@@ -1,7 +1,6 @@
 __author__ = 'kanaan... 28.03.2017'
 
-import os.path.isfile as exists
-import os.path.join as join
+import os.path as op
 from variables.subject_list import tourettome_subjects, tourettome_workspace
 
 
@@ -9,7 +8,7 @@ from variables.subject_list import tourettome_subjects, tourettome_workspace
 def check_outputs(population, workspace):
 
     for subject in population:
-        if exists(join(workspace, subject, 'ANATOMICAL/ANATOMICAL_GM.nii.gz')):
+        if op.isfile(op.join(workspace, subject, 'ANATOMICAL/ANATOMICAL_GM.nii.gz')):
             print '%s Anatomical complete' %subject
         else:
             print 'Subject %s anatomical preprocessing still running'
