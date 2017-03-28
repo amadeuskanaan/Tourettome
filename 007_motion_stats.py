b2a88ab76748ae006472c2254c59db5e1a0c83c8
kanaan <amadeus.kanaan@gmail.com>
2017-03-28 13:06:43 +0200

    initial commit

--- 007_motion_stats.py
+++ 007_motion_stats.py
@@ -2,14 +2,13 @@ __author__ = 'kanaan 23.03.2017'
 
 import os
 import numpy as np
-
 from utilities.utils import *
 from variables.subject_list import *
 from motion.motion_statistics import *
+from nipype.algorithms.misc import TSNR
 
 
-
-def generate_motion_stats(population, workspace):
+def quality_control(population, workspace):
 
 
     for subject in population:
@@ -19,22 +18,40 @@ def generate_motion_stats(population, workspace):
 
 
         #input
-        subdir = os.path.join(workspace, subject)
-        movpar = os.path.join(subdir, 'FUNCTIONAL/moco/REST_EDIT_moco2.1D')
-        maxdsp = os.path.join(subdir, 'FUNCTIONAL/moco/REST_EDIT_moco2_MX.1D')
-        func_pproc   = os.path.join(subdir, 'FUNCTIONAL/moco/REST_EDIT_moco2_MX.1D')
-        func_denoise = os.path.join(subdir, 'FUNCTIONAL/moco/REST_EDIT_moco2_MX.1D')
+        subdir       = os.path.join(workspace, subject)
+        movpar       = os.path.join(subdir, 'FUNCTIONAL', 'moco/REST_EDIT_moco2.par')
+        maxdsp       = os.path.join(subdir, 'FUNCTIONAL', 'moco/REST_EDIT_moco2_MX.1D')
+        gm           = os.path.join(subdir, 'REGISTRATION', 'ANATOMICAL_GM_MNI2mm.nii.gz')
+        func_edit    = os.path.join(subdir, 'FUNCTIONAL', 'REST_EDIT_MOCO_BRAIN.nii.gz')
+        func_wmcsf   = os.path.join(subdir, 'DENOISE', 'REST_MNI2mm_detrend_wmcsf_moco24_bp.nii.gz')
+        func_compcor = os.path.join(subdir, 'DENOISE', 'REST_MNI2mm_detrend_compcor_moco24_bp.nii.gz')
+        func_aroma   = os.path.join(subdir, 'DENOISE', 'REST_MNI2mm_fwhm_aroma_detrend_compcor_moco24_bp.nii.gz')
 
         #output
         qcdir = mkdir_path(os.path.join(subdir, 'QUALITY'))
         os.chdir(qcdir)
 
-        #calculate Framewise-Displacment
-        calculate_FD_Power(movpar)
+        #######################################
+        # Calculate Framewise-Displacment
+        FD1D = np.loadtxt(calculate_FD_Power(movpar))
+        frames_in = [ frame for frame, val in enumerate(FD1D) if val<0.2]
 
-        #calculate DVARS
-        calculate_DVARS()
+        print frames_in
+
+        #######################################
+        # Calculate DVARS
+        if not os.path.isfile(DVARS):
+            DVARS = calculate_DVARS(func_wmcsf, gm)
+
+        #######################################
+        if not os.path.isfile(os.path.join(qcdir, 'REST_PPROC_NATIVE_BRAIN_tsnr.nii.gz')):
+            tsnr = TSNR()
+            tsnr.inputs.in_file = func_edit
+            tsnr.run()
 
 
 
-generate_motion_stats(['PA013'], tourettome_workspace)
+
+
+
+quality_control(['PA072'], tourettome_workspace)
