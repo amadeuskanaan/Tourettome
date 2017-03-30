0552d4d553fa4fefd716966ca58d712b8e8edcd6
amadeuskanaan <amadeus.kanan@gmail.com>
2017-03-30 16:34:41 -0400

    push

---
+++ 008_reports.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import nibabel as nb
+import matplotlib
+
+
+
+
+###########
+# ANATOMICAL
+# 1- RAW
+# 2- Deskulled
+# 3- GM/WM/CSF
+# 4- Reg to mni
+
+###########
+# Functional
+# 1- Raw Mean
+# 2- TSNR
+# 3- FD
+# 4- DVARS
+# 5- denoised imshow
+
+
+# GM/WM/CSF
+# PLOT FUNCTIONAL MEAN ax-cor-sagg at level of BG
+
+
+
+
+# make qap pipe
