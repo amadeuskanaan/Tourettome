d1acda9c5abb7145a20582b3d5519fc7199c2be7
amadeuskanaan <amadeus.kanan@gmail.com>
2017-03-30 18:41:14 -0400

    push

--- 008_reports.py
+++ 008_reports.py
@@ -1,7 +1,4 @@
 import os
-import numpy as np
-import nibabel as nb
-import matplotlib
 from variables.subject_list import *
 from utilities.utils import *
 from plots.plot_volumes import *
@@ -20,9 +17,9 @@ def make_quality_reports(population, workspace):
         qcdir  = mkdir_path(os.path.join(subdir, 'QUALITY'))
         os.chdir(qcdir)
 
-        anat      = os.path.join(subdir, 'ANATOMICAL', 'ANATOMICAL_BRAIN.nii.gz' )
-        gm        = os.path.join(subdir, 'ANATOMICAL', 'ANATOMICAL_GM.nii.gz')
-        gm2mni    = os.path.join(subdir, 'REGISTRATION','ANATOMICAL_GM_MNI1mm.nii.gz')
+        anat      = os.path.join(subdir, 'ANATOMICAL',   'ANATOMICAL_BRAIN.nii.gz' )
+        gm        = os.path.join(subdir, 'ANATOMICAL',   'ANATOMICAL_GM.nii.gz')
+        gm2mni    = os.path.join(subdir, 'REGISTRATION', 'ANATOMICAL_GM_MNI1mm.nii.gz')
         func2anat = os.path.join(subject,'REGISTRATION', 'REST_EDIT_MOCO_BRAIN_MEAN_BBR_ANAT1mm.nii.gz')
 
         # Plot native anatomical with GM
@@ -34,15 +31,9 @@ def make_quality_reports(population, workspace):
         # Plot func2anat reg quality using GM as a boundary
         plot_vol_quality(func2anat, gm, subject[0:2], '%s - Func to Anat xfm' %subject, 'plot_func2anat,png', cmap = 'r' )
 
+
+
 make_quality_reports(['HA013'], tourettome_workspace)
-
-
-###########
-# ANATOMICAL
-# 1- RAW
-# 2- Deskulled
-# 3- GM/WM/CSF
-# 4- Reg to mni
 
 ###########
 # Functional
@@ -52,10 +43,8 @@ make_quality_reports(['HA013'], tourettome_workspace)
 # 4- DVARS
 # 5- denoised imshow
 
-
 # GM/WM/CSF
 # PLOT FUNCTIONAL MEAN ax-cor-sagg at level of BG
-
 
 
 
