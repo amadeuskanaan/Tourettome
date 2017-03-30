49043dd8d3f102dda7ab3457148c63085e0c3b84
amadeuskanaan <amadeus.kanan@gmail.com>
2017-03-30 05:13:27 +0200

    push

--- 003_kelly_kapowski.py
+++ 003_kelly_kapowski.py
@@ -2,6 +2,7 @@ import os
 
 from utilities.utils import mkdir_path
 from variables.subject_list import *
+from nipype.interfaces.ants.segmentation import KellyKapowski
 
 
 # Registration based cortical thickness measurement.
@@ -25,23 +26,39 @@ def make_cortical_thickness(population, workspace):
         prob_wm = os.path.join(subdir, 'ANATOMICAL/seg_spm/c2ANATOMICAL.nii')
         first   = os.path.join(subdir, 'ANATOMICAL/seg_first/FIRST.nii.gz')
 
-        # combine gm and wm with correect labels for DIRECT
 
-        os.system('fslmaths %s -thr 0.5 -bin -sub %s -bin -mul 2 gm' %(prob_gm, first))
-        os.system('fslmaths %s -thr 0.5 -bin -add %s -bin -mul 3 wm' %(prob_wm, first))
-        os.system('fslmaths gm -add wm segmentation0.nii.gz')
+        if not os.path.isfile(os.path.join(kellydir, 'segmentation0_cortical_thickness.nii.gz')):
 
-        # Run ANTS-DIRECT
-        os.system('KellyKapowski '
-                  '--image-dimensionality 3 '
-                  '--segmentation-image "[segmentation0.nii.gz,2,3]" '
-                  '--convergence "[45,0.0,10]" '
-                  '--output "[segmentation0_cortical_thickness.nii.gz, segmentation0_warped_white_matter.nii.gz]" '
-                  '--gradient-step 0.025000 '
-                  '--number-of-integration-points 10 '
-                  '--smoothing-variance 1.000000 '
-                  '--smoothing-velocity-field-parameter 1.500000 '
-                  '--thickness-prior-estimate 10.000000')
+            # combine gm and wm with correect labels for DIRECT
+
+            os.system('fslmaths %s -thr 0.5 -bin -sub %s -bin -mul 2 gm' %(prob_gm, first))
+            os.system('fslmaths %s -thr 0.5 -bin -add %s -bin -mul 3 wm' %(prob_wm, first))
+            os.system('fslmaths gm -add wm segmentation0.nii.gz')
+
+            # Run ANTS-DIRECT
+            # os.system('KellyKapowski '
+            #          '--image-dimensionality 3 '
+            #          '--segmentation-image "[segmentation0.nii.gz,2,3]" '
+            #          '--convergence "[45,0.0,10]" '
+            #          '--output "[segmentation0_cortical_thickness.nii.gz, segmentation0_warped_white_matter.nii.gz]" '
+            #          '--gradient-step 0.025000 '
+            #          '--number-of-integration-points 10 '
+            #          '--smoothing-variance 1.000000 '
+            #          '--smoothing-velocity-field-parameter 1.500000 '
+            #          '--thickness-prior-estimate 10.000000')
+
+            kk = KellyKapowski()
+            kk.inputs.dimension = 3
+            kk.inputs.segmentation_image = "[segmentation0.nii.gz,2,3]"
+            kk.inputs.convergence = "[45,0.0,10]"
+            kk.inputs.gradient_step = 0.025
+            kk.inputs.smoothing_variance = 1.0
+            kk.inputs.smoothing_velocity_field = 1.5
+            #kk.inputs.use_bspline_smoothing = False
+            kk.inputs.number_integration_points = 10
+            kk.inputs.thickness_prior_estimate = 10
+            kk.inputs.num_threads = 26
+            kk.run()
 
 
-make_cortical_thickness(paris[0:40], tourettome_workspace)
+make_cortical_thickness(['PA072'], tourettome_workspace)
