d14fd5883528aca0d3cd198b047715d2247a5825
amadeuskanaan <amadeus.kanan@gmail.com>
2017-04-02 07:56:51 -0400

    push

--- tourettome_check_reconall.py
+++ tourettome_check_reconall.py
@@ -7,39 +7,21 @@ print '================================================================'
 print '                  Tourettome  Population                        '
 
 # print sample sizes
+print '---------------------------------------------'
+
 print 'Tourettome - Leipzig      N=', len(leipzig)
 print 'Tourettome - Paris        N=', len(paris)
 print 'Tourettome - Hannover_a   N=', len(hannover_a)
 print 'Tourettome - Hannover_b   N=', len(hannover_b)
 
-# # check recon-all outputs
-# fsdir= tourettome_freesurfer
-# population= [i for i in sorted(os.listdir(fsdir)) if os.path.isdir(os.path.join(fsdir, i))]
-# missing= [sub for sub in population if 'aparc.DKTatlas+aseg.mgz' not in os.listdir(os.path.join(fsdir, sub, 'mri'))]
-#
-# print ''
-# print 'These subjects have an incomplete recon-all directory. Run them again.',  missing
-# print 'N=', len(missing)
-#
+print '---------------------------------------------'
 
 
+# # check FREESURFER DIR
+fsdir= tourettome_freesurfer
+missing= [sub for sub in tourettome_freesurfer if 'aparc.DKTatlas+aseg.mgz' not in os.listdir(os.path.join(fsdir, sub, 'mri'))]
 
-def run_reconchecker(population, fs_dir):
+print ''
+print 'These subjects have an incomplete recon-all directory. Run them again.',  missing
+print 'N=', len(missing)
 
-    for subject in population:
-
-        os.system('$QA_TOOLS/recon_checker '
-                  '-s %s '
-                  '-snaps-out '
-                  '-snaps-detailed '
-                  '-gen-outputFOF '
-                  %(subject))
-
-
-
-
-
-
-
-
-run_thick2surf(tourettome_subjects, tourettome_freesurfer)
