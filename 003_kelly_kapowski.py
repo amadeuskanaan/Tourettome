303d54932969cc63a55c983ae8c14f7e566d1604
amadeuskanaan <amadeus.kanan@gmail.com>
2017-04-02 09:28:32 -0400

    push

--- 003_kelly_kapowski.py
+++ 003_kelly_kapowski.py
@@ -80,12 +80,9 @@ def make_cortical_thickness(population, workspace, freesurfer_dir, num_threads =
                       '--regheader' %(T1mgz))
 
 
-make_cortical_thickness(population=['HA022'], workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
+# make_cortical_thickness(population=['HA022'], workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
+make_cortical_thickness(population=paris1, workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
+# make_cortical_thickness(population=paris2, workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
+# make_cortical_thickness(population=hannover_a1, workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
+# make_cortical_thickness(population=hannover_a2, workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
 
-#make_cortical_thickness(population=paris1, workspace=tourettome_workspace)
-#make_cortical_thickness(population=paris2, workspace=tourettome_workspace)
-#make_cortical_thickness(population=leipzig1, workspace=tourettome_workspace)
-#make_cortical_thickness(population=leipzig2, workspace=tourettome_workspace)
-#make_cortical_thickness(population=hannover_a1, workspace=tourettome_workspace)
-#make_cortical_thickness(population=hannover_a2, workspace=tourettome_workspace)
-#make_cortical_thickness(population=hannover_b, workspace=tourettome_workspace)
