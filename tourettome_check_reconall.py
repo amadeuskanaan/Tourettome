f783d6e741d23cb5e572c0458a93a75b3e105db3
amadeuskanaan <amadeus.kanan@gmail.com>
2017-04-02 08:14:21 -0400

    push

--- tourettome_check_reconall.py
+++ tourettome_check_reconall.py
@@ -22,13 +22,8 @@ fsdir= tourettome_freesurfer
 
 
 
-for sub in os.listdir(fsdir):
-    if sub in tourettome_subjects:
-        if 'aparc.DKTatlas+aseg.mgz' not in os.listdir(os.path.join(fsdir, sub, 'mri')):
-            print sub
-
-
-missing_files = [sub for sub in os.listdir(fsdir) if sub in tourettome_subjects if 'aparc.DKTatlas+aseg.mgz' not in os.listdir(os.path.join(fsdir, sub, 'mri'))]
+fsdir_subs    = [sub for sub in os.listdir(fsdir) if sub in tourettome_subjects]
+missing_files = [sub for sub in fsdir_subs if 'aparc.DKTatlas+aseg.mgz' not in os.path.join(fsdir, sub, 'mri')]
 missing_dirs  = [sub for sub in tourettome_subjects if not os.path.isdir(os.path.join(fsdir, sub))]
 
 print ''
