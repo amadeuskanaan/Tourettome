6a2e2ed992fe5149cd732a7583149aa963c6cbc3
amadeuskanaan <amadeus.kanan@gmail.com>
2017-04-02 08:08:38 -0400

    push

--- tourettome_check_reconall.py
+++ tourettome_check_reconall.py
@@ -24,6 +24,7 @@ missing_files = [sub for sub in os.listdir(fsdir) if sub in tourettome_subjects 
 missing_dirs  = [sub for sub in tourettome_subjects if not os.path.isdir(os.path.join(fsdir, sub))]
 
 print ''
-print 'These subjects have an incomplete recon-all directory. Run them again.',  missing_files + missing_dirs
+print 'These subjects have no recon-all directory.',  missing_dirs
+print 'These subjects have an incomplete recon-all directory.',  missing_files
 print 'N=', len(missing_files + missing_dirs)
 
