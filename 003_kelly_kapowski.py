e7525717e044f810871f278422a0c9407785edfb
amadeuskanaan <amadeus.kanan@gmail.com>
2017-04-02 09:30:44 -0400

    push

--- 003_kelly_kapowski.py
+++ 003_kelly_kapowski.py
@@ -82,7 +82,7 @@ def make_cortical_thickness(population, workspace, freesurfer_dir, num_threads =
 
 # make_cortical_thickness(population=['HA022'], workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
 # make_cortical_thickness(population=paris1, workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
-make_cortical_thickness(population=paris2, workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
-# make_cortical_thickness(population=hannover_a1, workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
+# make_cortical_thickness(population=paris2, workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
+make_cortical_thickness(population=hannover_a1, workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
 # make_cortical_thickness(population=hannover_a2, workspace=tourettome_workspace, freesurfer_dir=tourettome_freesurfer, num_threads=100)
 
