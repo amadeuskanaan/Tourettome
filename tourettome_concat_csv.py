a02429451bff8c69e8e7ff74676b496f078eb9e8
amadeuskanaan <amadeus.kanan@gmail.com>
2017-04-06 16:37:45 -0400

    push

--- tourettome_concat_csv.py
+++ tourettome_concat_csv.py
@@ -7,7 +7,7 @@ from variables.subject_list import *
 def concat_csv(population, workspace, phenotypic_dir):
 
     def get_dcm_header(site_id):
-        df = pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_%s.csv'%site_id))
+        df = pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_%s.csv'%site_id), index_col = 0)
         df = df[['Group', 'Site', 'Age', 'Sex']]
         return df
 
@@ -16,8 +16,8 @@ def concat_csv(population, workspace, phenotypic_dir):
                         get_dcm_header('leipzig'),
                         get_dcm_header('paris') ])
     # Concat Basal Ganglia bin_count dataframes
-    df_count = pd.concat([pd.read_csv(os.path.join(workspace, subject, 'ANATOMICAL','seg_first', 'bin_count.csv'))
-                          for subject in population], ignore_index = 0)
+    df_count = pd.concat([pd.read_csv(os.path.join(workspace, subject, 'ANATOMICAL','seg_first', 'bin_count.csv'), index_col = 0)
+                          for subject in population], ignore_index = False)
 
     for roi in rois_bilateral:
         df_count['%s' %roi]  = df_count['L_%s'%roi] + df_count['R_%s'%roi]
