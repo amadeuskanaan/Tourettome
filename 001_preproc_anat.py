577c31d1a90706e133ff3f4f7d0cbbeefcf72ee9
amadeuskanaan <amadeus.kanan@gmail.com>
2017-04-06 16:05:52 -0400

    push

--- 001_preproc_anat.py
+++ 001_preproc_anat.py
@@ -96,6 +96,7 @@ def preprocess_anatomical(population, workspace):
         # create bilateral masks
         if not os.path.isfile(os.path.join(anatdir, 'seg_first/Thal.nii.gz')):
             for roi in ['Caud', 'Puta', 'Pall',  'Amyg', 'Hipp', 'Accu', 'Thal']:
+                os.chdir(firstdir)
                 os.system('fslmaths FIRST-R_%s_first.nii.gz -add FIRST-L_%s_first.nii.gz -bin -out %s' %(roi, roi, roi))
 
         # make count
