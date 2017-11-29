58331fc2cf23415085fb6d91ff43ab6b32ae5892
amadeuskanaan <amadeus.kanaan@gmail.com>
2017-11-29 18:44:32 +0100

    push

--- 000_check_raw_data.py
+++ 000_check_raw_data.py
@@ -19,16 +19,14 @@ def preprocess_anatomical(population, afs_dir, workspace):
         print '-%s Preprocessing anatomical data for %s' %(count, subject)
 
         data_dir = os.path.join(afs_dir, subject, 'NIFTI')
-        out_dir = mkdir_path(os.path.join(workspace, subject, 'RAW'))
+        raw_dir = mkdir_path(os.path.join(workspace, subject, 'RAW'))
 
-        if not os.path.isfile(os.path.join(out_dir, 'ANATOMICAL.nii.gz')):
-            shutil.copy(os.path.join(data_dir, 'ANATOMICAL.nii.gz'), os.path.join(out_dir, 'ANATOMICAL.nii.gz'))
+        if not os.path.isfile(os.path.join(raw_dir, 'ANATOMICAL.nii.gz')):
+            shutil.copy(os.path.join(data_dir, 'ANATOMICAL.nii.gz'), raw_dir)
 
-        if not os.path.isfile(os.path.join(out_dir, 'REST.nii.gz')):
-            shutil.copy(os.path.join(data_dir, 'REST.nii.gz'), os.path.join(out_dir, 'REST.nii.gz'))
+        if not os.path.isfile(os.path.join(raw_dir, 'REST.nii.gz')):
+            shutil.copy(os.path.join(data_dir, 'REST.nii.gz'), raw_dir)
 
-        if subject[0:2] == 'LZ':
-            if not os.path.isfile(os.path.join(out_dir, 'T1MAPS.nii.gz')):
-                shutil.copy(os.path.join(data_dir, 'T1MAPS.nii.gz'), os.path.join(out_dir, 'T1MAPS.nii.gz'))
-
-preprocess_anatomical(population = tourettome_subjects, afs_dir = tourettome_afs, workspace = tourettome_workspace)
+preprocess_anatomical(population = tourettome_subjects,
+                      afs_dir    = tourettome_afs,
+                      workspace  = tourettome_workspace)
