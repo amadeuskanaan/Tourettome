0cb9925b20125936f2d23c25c074eb14e4810075
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-02 23:01:53 -0500

    push

--- 007_make_derivatives_struct.py
+++ 007_make_derivatives_struct.py
@@ -50,11 +50,42 @@ def make_derivatives_struct(population, workspace_dir, freesurfer_dir, derivativ
                          '--noreshape '
                          ]
 
-            print ' '.join(surf2surf)
+            print os_system(surf2surf)
 
         print '##################################'
         print '2- Extracting Geodesic Distance'
 
+        print '##################################'
+        print '3- Extracting Surface Area'
+
+        print '##################################'
+        print '4- Extracting Intensity Contrast'
+
+        print '##################################'
+        print '5- Extracting Subcortical Volumes'
+
+        # ####### Count number of non-zero voxels for FSL-FIRST subcortical segmentations
+        #
+        # # create bilateral masks
+        # for roi in ['Caud', 'Puta', 'Pall',  'Amyg', 'Hipp', 'Accu', 'Thal']:
+        #     if not os.path.isfile(os.path.join(anatdir, 'seg_first/FIRST-%s_first.nii.gz'%roi)):
+        #         os.chdir(firstdir)
+        #         os.system('fslmaths FIRST-R_%s_first.nii.gz -add FIRST-L_%s_first.nii.gz -bin FIRST-%s_first.nii.gz' %(roi, roi, roi))
+        #
+        # # Get jacobian deteminant from anat2mni.mat and multiply by bincount
+        # jacobian_det = np.linalg.det(np.genfromtxt(os.path.join(anatdir, 'seg_first', 'anat2mni.mat')))
+        # print jacobian_det
+        #
+        # # Make count
+        # df = pd.DataFrame(index = ['%s'%subject], columns = rois)
+        # if not os.path.isfile(os.path.join(anatdir, 'seg_first/first_count_jac.csv')):
+        #     for roi in rois:
+        #         first = os.path.join(firstdir,'FIRST-%s_first.nii.gz' %roi )
+        #         count = np.count_nonzero(nb.load(first).get_data()) * jacobian_det
+        #         df.ix['%s'%subject, roi] = count
+        #
+        # df.to_csv(os.path.join(firstdir, 'bin_count_jac.csv'))
+        # print df
 
 
 make_derivatives_struct(['PA005'], tourettome_workspace, tourettome_freesurfer, tourettome_derivatives )
