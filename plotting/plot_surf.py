845d0b5e92424774d166b707b00b976b75c1f047
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-03 19:39:35 -0500

    initial commit

---
+++ plotting/plot_surf.py
@@ -0,0 +1,136 @@
+
+
+def plot_surf_stat_all(fsaverage,
+                       surf_map_l,
+                       surf_map_r,
+                       threshold=None,
+                       alpha=0.7,
+                       cmap='hot',
+                       output_file=None, title=None):
+    """
+    Functions for surface visualization.
+    Only matplotlib is required.
+    """
+
+    # Import libraries
+    import nibabel
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    from mpl_toolkits.mplot3d import Axes3D
+    from nibabel import gifti
+
+    from nilearn._utils.compat import _basestring
+    from nilearn.plotting.img_plotting import _get_colorbar_and_data_ranges
+    from nilearn.plotting.surf_plotting import load_surf_data, load_surf_mesh
+
+
+    # load mesh and derive axes limits
+    mesh_left = load_surf_mesh(fsaverage['pial_left'])
+    mesh_right = load_surf_mesh(fsaverage['pial_right'])
+
+    coords_left, faces_left = mesh_left[0], mesh_left[1]
+    coords_right, faces_right = mesh_right[0], mesh_right[1]
+
+    limits = [-65, 33]
+
+    avg_method = 'mean'
+    darkness = 1
+
+    # if no cmap is given, set to matplotlib default
+    if cmap is None:
+        cmap = plt.cm.get_cmap(plt.rcParamsDefault['image.cmap'])
+    else:
+        # if cmap is given as string, translate to matplotlib cmap
+        if isinstance(cmap, _basestring):
+            cmap = plt.cm.get_cmap(cmap)
+
+    def subplot(surf_map, bg_map, subplot_id, coords, faces, elev, azim, vmin=None, vmax=None):
+
+        bg_on_data = False
+
+        ax = fig.add_subplot(subplot_id, projection='3d', xlim=limits, ylim=limits)
+        ax.view_init(elev=elev, azim=azim)
+        ax.set_axis_off()
+        p3dcollec = ax.plot_trisurf(coords[:, 0], coords[:, 1], coords[:, 2], triangles=faces, linewidth=0.,
+                                    antialiased=False, color='red')
+
+        # If depth_map and/or surf_map are provided, map these onto the surface
+        # set_facecolors function of Poly3DCollection is used as passing the
+        # facecolors argument to plot_trisurf does not seem to work
+        if bg_map is not None or surf_map is not None:
+
+            face_colors = np.ones((faces.shape[0], 4))
+            # face_colors[:, :3] = .5*face_colors[:, :3]  # why this?
+
+            if bg_map is not None:
+                bg_data = load_surf_data(bg_map)
+                if bg_data.shape[0] != coords.shape[0]:
+                    raise ValueError('The bg_map does not have the same number '
+                                     'of vertices as the mesh.')
+                bg_faces = np.mean(bg_data[faces], axis=1)
+                bg_faces = bg_faces - bg_faces.min()
+                bg_faces = bg_faces / bg_faces.max()
+                # control background darkness
+                bg_faces *= darkness
+                face_colors = plt.cm.gray_r(bg_faces)
+
+            # modify alpha values of background
+            face_colors[:, 3] = alpha * face_colors[:, 3]
+            # should it be possible to modify alpha of surf data as well?
+
+            if surf_map is not None:
+                surf_map_data = surf_map  # load_surf_data(surf_map)
+                if len(surf_map_data.shape) is not 1:
+                    raise ValueError('surf_map can only have one dimension but has'
+                                     '%i dimensions' % len(surf_map_data.shape))
+                if surf_map_data.shape[0] != coords.shape[0]:
+                    raise ValueError('The surf_map does not have the same number '
+                                     'of vertices as the mesh.')
+
+                # create face values from vertex values by selected avg methods
+                if avg_method == 'mean':
+                    surf_map_faces = np.mean(surf_map_data[faces], axis=1)
+                elif avg_method == 'median':
+                    surf_map_faces = np.median(surf_map_data[faces], axis=1)
+
+                # if no vmin/vmax are passed figure them out from data
+                if vmin is None:
+                    vmin = np.nanmin(surf_map_faces)
+                if vmax is None:
+                    vmax = np.nanmax(surf_map_faces)
+
+                # treshold if inidcated
+                if threshold is None:
+                    kept_indices = np.where(surf_map_faces)[0]
+                else:
+                    kept_indices = np.where(np.abs(surf_map_faces) >= threshold)[0]
+
+                surf_map_faces = surf_map_faces - vmin
+                surf_map_faces = surf_map_faces / (vmax - vmin)
+
+                # multiply data with background if indicated
+                if bg_on_data:
+                    face_colors[kept_indices] = cmap(surf_map_faces[kept_indices]) \
+                                                * face_colors[kept_indices]
+                else:
+                    face_colors[kept_indices] = cmap(surf_map_faces[kept_indices])
+
+            p3dcollec.set_facecolors(face_colors)
+
+    fig = plt.figure(figsize=(55, 20))
+
+    subplot(surf_map_l, fsaverage['sulc_left'], 141, coords_left, faces_left, elev=0, azim=180)
+    subplot(surf_map_l, fsaverage['sulc_left'], 142, coords_left, faces_left, elev=0, azim=0)
+    subplot(surf_map_r, fsaverage['sulc_right'], 143, coords_right, faces_right, elev=0, azim=180)
+    subplot(surf_map_r, fsaverage['sulc_right'], 144, coords_right, faces_right, elev=0, azim=0)
+
+    if title is not None:
+        plt.title(title)
+    plt.tight_layout()
+    # save figure if output file is given
+    if output_file is not None:
+        fig.savefig(output_file)
+        plt.close(fig)
+        # else:
+        #    return fig
