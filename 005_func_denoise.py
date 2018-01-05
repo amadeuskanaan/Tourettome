a7b1d7f2488fc8efa03fd7daf1ccee4d0fe07181
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-05 14:30:03 -0500

    push

--- 005_func_denoise.py
+++ 005_func_denoise.py
@@ -175,7 +175,8 @@ def nuisance_signal_regression(population, workspace_dir):
         #             gmsig =os.path.join(wmcsf_dir, 'gm_signals.npy'))
 
 
-nuisance_signal_regression(tourettome_subjects, tourettome_workspace)
+# nuisance_signal_regression(tourettome_subjects, tourettome_workspace)
+nuisance_signal_regression(paris, tourettome_workspace)
 # nuisance_signal_regression(['PA060'], tourettome_workspace)
 
 
