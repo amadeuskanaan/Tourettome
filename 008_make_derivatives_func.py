4b689e8259e879da14f4892d6803ded4f7890e65
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-04 23:00:13 -0500

    plt

--- 008_make_derivatives_func.py
+++ 008_make_derivatives_func.py
@@ -65,7 +65,7 @@ def make_functional_derivatives(population, workspace_dir, freesurfer_dir, deriv
 
         #I/0
         subject_dir     = os.path.join(workspace_dir, subject)
-        func_denoised   = os.path,join(subject_dir, 'DENOISE/residual_compcor/residual_bp_z_fwhm6.nii.gz')
+        func_denoised   = os.path.join(subject_dir, 'DENOISE/residual_compcor/residual_bp_z_fwhm6.nii.gz')
 
 
         print '##################################################################'
