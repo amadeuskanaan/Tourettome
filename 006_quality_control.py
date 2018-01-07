9c894c311a12da95ec5f2565a0f085752ff0acf3
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-06 22:59:32 -0500

    m

--- 006_quality_control.py
+++ 006_quality_control.py
@@ -166,4 +166,4 @@ def make_subject_qc(population, workspace):
                      'TSNR', '%s-func_tsnr' % subject, 'r', alpha=0.9, title='plot_func_tsnr.png')
 
 
-make_subject_qc(['PA020'], tourettome_workspace)
+make_subject_qc(hamburg+hannover_a+hannover_b, tourettome_workspace)
