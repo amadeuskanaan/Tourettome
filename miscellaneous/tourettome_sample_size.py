f89655180282bf62fcbadccd9c94e232e6e41d9e
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-06 23:32:18 -0500

    m

--- miscellaneous/tourettome_sample_size.py
+++ miscellaneous/tourettome_sample_size.py
@@ -1,9 +1,10 @@
 import os, sys
-sys.path.append(os.path.expanduser('/scr/malta1/Github/Tourettome/'))
+# sys.path.append(os.path.expanduser('/scr/malta1/Github/Tourettome/'))
 from variables.subject_list import *
 
 print 'PARIS len =', len(paris)
 print 'Leipzig len =', len(leipzig)
 print 'Hannover_a len =', len(hannover_a)
 print 'Hannover_b len =', len(hannover_b)
+print 'Hamburg len =', len(hamburg)
 print 'Tourettome subjects =', len(tourettome_subjects)
