b2e51d07e05d9188227f44a2227f10353a5c3ebb
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-11 16:35:06 -0500

    push

--- 007_concat_phenotypic_dfs.py
+++ 007_concat_phenotypic_dfs.py
@@ -8,7 +8,7 @@ from utilities.utils import *
 
 # Concatenate dataframes that include (a) DICOM Header, (b) clinical data, (c) quality control metrics.
 
-def concat_dataframes(population, workspace_dir, phenotypic_dir)
+def concat_dataframes(population, workspace_dir, phenotypic_dir):
 
     df_dcm =  pd.concat([pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_leipzig.csv'),index_col =0),
                          #pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_paris.csv'),index_col =0),
@@ -17,7 +17,7 @@ def concat_dataframes(population, workspace_dir, phenotypic_dir)
                          #pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_hamburg.csv'),index_col =0),
                          ])
 
-    df_cln = pd.concat([pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_leipzig.csv'),index_col =0)
+    df_cln = pd.concat([pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_leipzig.csv'),index_col =0),
                         # pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_paris.csv'), index_col=0)
                         # pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_hannover_a.csv'), index_col=0)
                         # pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_hannover_b.csv'), index_col=0)
@@ -31,8 +31,9 @@ def concat_dataframes(population, workspace_dir, phenotypic_dir)
     df_pheno = pd.concat([df_dcm, df_qc, df_cln], axis=1).sort_index()
     df_pheno.to_csv(os.path.join(phenotypic_dir, 'tourettome_phenotypic.csv'))
 
-#
-#
+concat_dataframes(tourettome_subjects, tourettome_workspace, tourettome_phenotypic)
+
+
 # import os
 # import pandas as pd
 # from variables.subject_list import *
@@ -57,4 +58,3 @@ def concat_dataframes(population, workspace_dir, phenotypic_dir)
 #     df_pheno = pd.concat([df_dcm, df_qc], axis=1).sort_index()
 #
 #
-# concat_csvs(tourettome_subjects, tourettome_workspace, tourettome_phenotypic)
