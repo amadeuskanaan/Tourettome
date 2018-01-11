4bd1fcad3f689f5aa2d61442f84ae50a5e1fa556
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-11 16:36:54 -0500

    push

--- 007_concat_phenotypic_dfs.py
+++ 007_concat_phenotypic_dfs.py
@@ -11,17 +11,17 @@ from utilities.utils import *
 def concat_dataframes(population, workspace_dir, phenotypic_dir):
 
     df_dcm =  pd.concat([pd.read_csv(os.path.join(phenotypic_dir, 'df_dcm/dicomhdr_leipzig.csv'),index_col =0),
-                         #pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_paris.csv'),index_col =0),
-                         #pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_hannover_a.csv'),index_col =0),
-                         #pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_hannover_b.csv'),index_col =0),
-                         #pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_hamburg.csv'),index_col =0),
+                         #pd.read_csv(os.path.join(phenotypic_dir, 'df_dcm/dicomhdr_paris.csv'),index_col =0),
+                         #pd.read_csv(os.path.join(phenotypic_dir, 'df_dcm/dicomhdr_hannover_a.csv'),index_col =0),
+                         #pd.read_csv(os.path.join(phenotypic_dir, 'df_dcm/dicomhdr_hannover_b.csv'),index_col =0),
+                         #pd.read_csv(os.path.join(phenotypic_dir, 'df_dcm/dicomhdr_hamburg.csv'),index_col =0),
                          ])
 
-    df_cln = pd.concat([pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_leipzig.csv'),index_col =0),
-                        # pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_paris.csv'), index_col=0)
-                        # pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_hannover_a.csv'), index_col=0)
-                        # pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_hannover_b.csv'), index_col=0)
-                        # pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_hamburg.csv'), index_col=0)
+    df_cln = pd.concat([pd.read_csv(os.path.join(phenotypic_dir, 'df_cln/clinical_leipzig.csv'),index_col =0),
+                        # pd.read_csv(os.path.join(phenotypic_dir, 'df_cln/clinical_paris.csv'), index_col=0)
+                        # pd.read_csv(os.path.join(phenotypic_dir, 'df_cln/clinical_hannover_a.csv'), index_col=0)
+                        # pd.read_csv(os.path.join(phenotypic_dir, 'df_cln/clinical_hannover_b.csv'), index_col=0)
+                        # pd.read_csv(os.path.join(phenotypic_dir, 'df_cln/clinical_hamburg.csv'), index_col=0)
                         ])
 
     df_qc = [pd.read_csv(os.path.join(workspace_dir, subject, 'QUALITY_CONTROL/quality_paramters.csv'),
