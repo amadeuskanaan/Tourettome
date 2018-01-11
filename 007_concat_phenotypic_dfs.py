4d5a9559ce503906e94a18c76682d9c6cb0d1b6f
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-11 16:23:48 -0500

    push

--- 007_concat_phenotypic_dfs.py
+++ 007_concat_phenotypic_dfs.py
@@ -8,7 +8,7 @@ from utilities.utils import *
 
 # Concatenate dataframes that include (a) DICOM Header, (b) clinical data, (c) quality control metrics.
 
-def concat_dataframes(population, workspace, phenotypic_dir)
+def concat_dataframes(population, workspace_dir, phenotypic_dir)
 
     df_dcm =  pd.concat([pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_leipzig.csv'),index_col =0),
                          #pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_paris.csv'),index_col =0),
@@ -17,41 +17,44 @@ def concat_dataframes(population, workspace, phenotypic_dir)
                          #pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_hamburg.csv'),index_col =0),
                          ])
 
-    #df_cln = pd.concat[[pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_leipzig.csv'),index_col =0),
-    #])
+    df_cln = pd.concat([pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_leipzig.csv'),index_col =0)
+                        # pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_paris.csv'), index_col=0)
+                        # pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_hannover_a.csv'), index_col=0)
+                        # pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_hannover_b.csv'), index_col=0)
+                        # pd.read_csv(os.path.join(phenotypic_dir, 'CLINICAL/clinical_hamburg.csv'), index_col=0)
+                        ])
 
+    df_qc = [pd.read_csv(os.path.join(workspace_dir, subject, 'QUALITY_CONTROL/quality_paramters.csv'),
+                         #index_col=0) for subject in df_cln]
+                         index_col = 0) for subject in df_dcm.index]
 
+    df_pheno = pd.concat([df_dcm, df_qc, df_cln], axis=1).sort_index()
+    df_pheno.to_csv(os.path.join(phenotypic_dir, 'tourettome_phenotypic.csv'))
 
-
-
-
-
-
-
-
-
-import os
-import pandas as pd
-from variables.subject_list import *
-
-def concat_csvs(population, workspace, phenotypic_dir):
-
-    def get_dcm_header(site_id):
-        df = pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_%s.csv'%site_id), index_col = 0)
-        df = df[['Group', 'Site', 'Age', 'Sex']]
-        return df
-
-    df_dcm = pd.concat([get_dcm_header('hannover_a'),
-                        get_dcm_header('hannover_b'),
-                        get_dcm_header('leipzig'),
-                        get_dcm_header('hamburg'),
-                        get_dcm_header('paris') ]
-                       )
-
-    df_qc = [pd.read_csv(os.path.join(workspace,subject, 'QUALITY_CONTROL/quality_paramters.csv'),
-                         index_col=0) for subject in population]
-
-    df_pheno = pd.concat([df_dcm, df_qc], axis=1).sort_index()
-
-
-concat_csvs(tourettome_subjects, tourettome_workspace, tourettome_phenotypic)
+#
+#
+# import os
+# import pandas as pd
+# from variables.subject_list import *
+#
+# def concat_csvs(population, workspace, phenotypic_dir):
+#
+#     def get_dcm_header(site_id):
+#         df = pd.read_csv(os.path.join(phenotypic_dir, 'phenotypic_%s.csv'%site_id), index_col = 0)
+#         df = df[['Group', 'Site', 'Age', 'Sex']]
+#         return df
+#
+#     df_dcm = pd.concat([get_dcm_header('hannover_a'),
+#                         get_dcm_header('hannover_b'),
+#                         get_dcm_header('leipzig'),
+#                         get_dcm_header('hamburg'),
+#                         get_dcm_header('paris') ]
+#                        )
+#
+#     df_qc = [pd.read_csv(os.path.join(workspace,subject, 'QUALITY_CONTROL/quality_paramters.csv'),
+#                          index_col=0) for subject in population]
+#
+#     df_pheno = pd.concat([df_dcm, df_qc], axis=1).sort_index()
+#
+#
+# concat_csvs(tourettome_subjects, tourettome_workspace, tourettome_phenotypic)
