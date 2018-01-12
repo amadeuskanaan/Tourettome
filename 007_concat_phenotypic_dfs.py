4cd31bb9be4ef888c83a1b54dff8510a3a2d3c27
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-11 19:07:54 -0500

    push

--- 007_concat_phenotypic_dfs.py
+++ 007_concat_phenotypic_dfs.py
@@ -37,36 +37,21 @@ def concat_dataframes(population, workspace_dir, phenotypic_dir):
     print '##############################################'
     print '2. Create design matrix dataframe for surfstat'
 
-    subsx = ['HA001', 'HA002', 'HA003', 'HA004', 'HA005', 'HA006', 'HA007', 'HA008', 'HA009', 'HA010', 'HA011', 'HA012',
-             'HA013', 'HA014', 'HA015', 'HA016', 'HA017', 'HA018', 'HA019', 'HA020', 'HA021', 'HA022', 'HA023', 'HA024',
-             'HA025', 'HA026', 'HA027', 'HA028', 'HA029', 'HA030', 'HA031', 'HA032', 'HA033', 'HA034', 'HA035', 'HA036',
-             'HA037', 'HA038', 'HA039', 'HA040', 'HA041', 'HA042', 'HA043', 'HA044', 'HA045', 'HA046', 'HA047', 'HA048',
-             'HA049', 'HA050', 'HA051', 'HA052', 'HA053', 'HA054', 'HB001', 'HB002', 'HB003', 'HB006', 'HB011', 'HB012',
-             'HB013', 'HB016', 'HB017', 'HB021', 'HB022', 'HB023', 'HB024', 'HB026', 'HB027', 'HB028', 'HB029', 'HB030',
-             # 'HM001', 'HM002', 'HM003', 'HM004', 'HM005', 'HM006', 'HM007', 'HM008', 'HM009', 'HM010', 'HM011', 'HM012',
-             # 'HM014', 'HM019', 'HM020', 'HM022', 'HM023', 'HM025', 'HM026', 'HM027', 'HM028', 'HM030', 'HM031',
-             'LZ004',
-             'LZ006', 'LZ007', 'LZ008', 'LZ009', 'LZ010', 'LZ011', 'LZ012', 'LZ013', 'LZ014', 'LZ015', 'LZ016', 'LZ017',
-             'LZ018', 'LZ019', 'LZ020', 'LZ021', 'LZ022', 'LZ023', 'LZ024', 'LZ025', 'LZ026', 'LZ027', 'LZ028', 'LZ029',
-             'LZ030', 'LZ031', 'LZ032', 'LZ033', 'LZ034', 'LZ035', 'LZ036', 'LZ037', 'LZ038', 'LZ039', 'LZ041', 'LZ042',
-             'LZ043', 'LZ045', 'LZ046', 'LZ047', 'LZ048', 'LZ049', 'LZ051', 'LZ055', 'LZ056', 'LZ060', 'LZ061', 'LZ062',
-             'LZ063', 'LZ064', 'LZ065', 'LZ066', 'LZ067', 'LZ068', 'LZ069', 'LZ070', 'LZ071', 'LZ072', 'LZ073', 'LZ074',
-             'LZ075', 'PA001', 'PA002', 'PA003', 'PA004', 'PA005', 'PA006', 'PA007', 'PA008', 'PA009', 'PA010', 'PA011',
-             'PA012', 'PA013', 'PA014', 'PA016', 'PA017', 'PA018', 'PA019', 'PA021', 'PA022', 'PA026', 'PA027', 'PA028',
-             'PA031', 'PA032', 'PA033', 'PA035', 'PA037', 'PA038', 'PA042', 'PA043', 'PA047', 'PA048', 'PA050', 'PA052',
-             'PA056', 'PA058', 'PA062', 'PA063', 'PA064', 'PA066', 'PA067', 'PA068', 'PA069', 'PA070', 'PA071', 'PA072',
-             'PA073', 'PA074', 'PA075', 'PA076', 'PA077', 'PA078', 'PA079', 'PA080', 'PA081', 'PA082', 'PA083', 'PA084',
-             'PA085', 'PA087', 'PA089', 'PA090', 'PA091', 'PA092', 'PA093', 'PA095', 'PA096']
+    xoutliers =  ['HB004', 'HB005', 'HB007', 'HB008', 'HB009', 'HB010', 'HB014', 'HB015', 'HB018', 'HB019', 'HB020',
+             'HB025', 'HB031', 'HB032', 'HB033', 'HM001', 'HM002', 'HM003', 'HM004', 'HM005', 'HM006', 'HM007',
+             'HM008', 'HM009', 'HM010', 'HM011', 'HM012', 'HM014', 'HM015', 'HM017', 'HM019', 'HM020', 'HM022',
+             'HM023', 'HM024', 'HM025', 'HM026', 'HM027', 'HM028', 'HM029', 'HM030', 'HM031', 'HM032', 'HM033',
+             'LZ001', 'LZ002', 'LZ003', 'LZ005', 'LZ040', 'LZ044', 'LZ050', 'LZ052', 'LZ053', 'LZ054', 'LZ057',
+             'LZ058', 'LZ059', 'LZ076', 'PA015', 'PA020', 'PA023', 'PA024', 'PA025', 'PA029', 'PA030', 'PA036',
+             'PA039', 'PA040', 'PA041', 'PA044', 'PA045', 'PA046', 'PA049', 'PA051', 'PA053', 'PA054', 'PA055',
+             'PA059', 'PA060', 'PA061', 'PA088', 'PA094']
 
     design_columns = ['Group', 'Age', 'Sex', 'Site', 'qc_func_fd']
     df_design = df_pheno.drop([i for i in df_pheno.columns if i not in design_columns], axis=1)
 
-    tourettome_outliers = [i for i in df_pheno.index if i not in subsx]
-
     print 'dropping outliers=', tourettome_outliers
-    df_design.drop(tourettome_outliers)
-    df_design.to_csv(os.path.join(phenotypic_dir, 'tourettome_phenotypic_designx.csv'))
-
+    df_design.drop(xoutliers, index=0)
+    df_design.to_csv(os.path.join(phenotypic_dir, 'tourettome_phenotypic_design.csv'))
 
 concat_dataframes(tourettome_subjects, tourettome_workspace, tourettome_phenotypic)
 
