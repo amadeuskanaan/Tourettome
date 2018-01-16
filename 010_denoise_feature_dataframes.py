bf09673d9e0e3a7cc4a37de38a9ecb1e7b48ae3b
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-15 19:19:03 -0500

    push

--- 010_denoise_feature_dataframes.py
+++ 010_denoise_feature_dataframes.py
@@ -74,12 +74,12 @@ def regress_covariates(df_features, df_pheno, population, popname, features_dir,
         df_features_resid.append(model.fit().resid)
 
     # save residual data
-    df_features_resid = pd.concat(df_features_resid, axis=1)
+    df_features_resid = pd.concat(df_features_resid, axis=1).T
     df_features_resid.to_csv('%s/sca_%s_resid.csv' % (features_dir, popname))
 
     # plot residual data
     f = plt.figure(figsize=(12, 10))
-    sns.heatmap(df_features_resid.T, xticklabels=False, yticklabels=False, cmap=cmap, vmin=-.7, vmax=0.7)
+    sns.heatmap(df_features_resid, xticklabels=False, yticklabels=False, cmap=cmap, vmin=-.7, vmax=0.7)
     plt.savefig('%s/sca_%s_resid.png' % (features_dir, popname), bbox_inches='tight')
 
     return df_features_resid
@@ -163,7 +163,7 @@ def construct_features_dataframe(control_outliers, patient_outliers, workspace_d
         sca_controls_resid = pd.read_csv(os.path.join(features_dir, 'sca_controls_resid.csv'), index_col=0)
         sca_patients_resid = pd.read_csv(os.path.join(features_dir, 'sca_patients_resid.csv'), index_col=0)
 
-    print sca_controls_resid.T.shape
+    print sca_patients_resid.T.shape
 
     ############################################################################################################
     print ' ... z-scoring dataframes to control distribution'
@@ -172,15 +172,17 @@ def construct_features_dataframe(control_outliers, patient_outliers, workspace_d
 
     if not os.path.isfile(os.path.join(features_dir, 'sca_patients_resid_z.csv')):
         # Calculate control mu/sd across each vertex
-        n_vertices = sca_controls_resid.shape[0]
-        vertex_mu = [np.mean(sca_controls_resid.loc[vertex]) for vertex in range(n_vertices)]
-        vertex_sd = [np.std(sca_controls_resid.loc[vertex]) for vertex in range(n_vertices)]
+        n_vertices = sca_controls_resid.shape[1]
+        vertex_mu = [np.mean(sca_controls_resid.T.loc[vertex]) for vertex in range(n_vertices)]
+        vertex_sd = [np.std(sca_controls_resid.T.loc[vertex]) for vertex in range(n_vertices)]
 
         # Normalize dataframes
-        sca_controls_resid_z = pd.concat([(sca_controls_resid.loc[vertex] - vertex_mu[vertex]) /
+        sca_controls_resid_z = pd.concat([(sca_controls_resid.T.loc[vertex] - vertex_mu[vertex]) /
                                  vertex_sd[vertex] for vertex in range(n_vertices)],axis=1)
-        sca_patients_resid_z = pd.concat([(sca_patients_resid.loc[vertex] - vertex_mu[vertex]) /
+        sca_patients_resid_z = pd.concat([(sca_patients_resid.T.loc[vertex] - vertex_mu[vertex]) /
                                  vertex_sd[vertex] for vertex in range(n_vertices)],axis=1)
+
+        print sca_patients_resid_z.shape
 
         sca_controls_resid_z.to_csv('%s/sca_controls_resid_z.csv'%features_dir)
         sca_patients_resid_z.to_csv('%s/sca_patients_resid_z.csv'%features_dir)
