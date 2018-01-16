f0a29f8ebadaf27311c66bb00fbef983a47f4b81
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-15 19:51:01 -0500

    push

--- 010_denoise_feature_dataframes.py
+++ 010_denoise_feature_dataframes.py
@@ -134,14 +134,14 @@ def construct_features_dataframe(control_outliers, patient_outliers, workspace_d
 
         sca_controls_resid_z = pd.concat([(sca_controls_resid.T.loc[vertex] - vertex_mu[vertex]) /
                                  vertex_sd[vertex] for vertex in range(n_vertices)],axis=1).T
-        # sca_patients_resid_z = pd.concat([(sca_patients_resid.T.loc[vertex] - vertex_mu[vertex]) /
-        #                          vertex_sd[vertex] for vertex in range(n_vertices)],axis=1).T
+        sca_patients_resid_z = pd.concat([(sca_patients_resid.T.loc[vertex] - vertex_mu[vertex]) /
+                                 vertex_sd[vertex] for vertex in range(n_vertices)],axis=1).T
 
         # Save data frames
         sca_controls_resid_z.to_csv('%s/sca_controls_resid_z.csv'%features_dir)
-        # sca_patients_resid_z.to_csv('%s/sca_patients_resid_z.csv'%features_dir)
+        sca_patients_resid_z.to_csv('%s/sca_patients_resid_z.csv'%features_dir)
         plot_heatmap(sca_controls_resid_z, '%s/sca_controls_resid_z' % features_dir, vmin =-3, vmax=3, cmap = cmap_gradient)
-        # plot_heatmap(sca_patients_resid_z, '%s/sca_patients_resid_z' % features_dir, vmin =-3, vmax=3, cmap = cmap_gradient)
+        plot_heatmap(sca_patients_resid_z, '%s/sca_patients_resid_z' % features_dir, vmin =-3, vmax=3, cmap = cmap_gradient)
 
 
 
