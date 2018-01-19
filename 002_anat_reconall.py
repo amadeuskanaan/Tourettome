2267d4e6e2dade3b3f02587ac6c09b99097eb286
amadeuskanaan <amadeus.kanaan@gmail.com>
2018-01-19 21:47:08 +0100

    push

--- 002_anat_reconall.py
+++ 002_anat_reconall.py
@@ -90,7 +90,8 @@ tourettome_derivatives = '/scr/malta4/workspace/project_TOURETTOME/derivatives'
 
 all_pops = leipzig+paris+hamburg+hannover_b+hannover_a
 
-preprocess_anatomical(population =['HM029'], afs_dir = tourettome_afs, workspace = tourettome_workspace, freesurfer_dir= fsdir)
+preprocess_anatomical(population =['LZ044'], afs_dir = tourettome_afs, workspace = tourettome_workspace, freesurfer_dir= fsdir)
+#preprocess_anatomical(population =['HM029'], afs_dir = tourettome_afs, workspace = tourettome_workspace, freesurfer_dir= fsdir)
 #preprocess_anatomical(population =['PA009'], afs_dir = tourettome_afs, workspace = tourettome_workspace, freesurfer_dir= fsdir)
 #preprocess_anatomical(population =['HB015'], afs_dir = tourettome_afs, workspace = tourettome_workspace, freesurfer_dir= fsdir)
 #preprocess_anatomical(population =['PA055'], afs_dir = tourettome_afs, workspace = tourettome_workspace, freesurfer_dir= fsdir)
