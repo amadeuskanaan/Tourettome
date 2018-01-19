8b22a47827d14cfe1ee525a63b73080d33d18828
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-18 20:53:33 -0500

    push

--- utilities/check_data.py
+++ utilities/check_data.py
@@ -33,9 +33,12 @@ def return_ct_data(population, derivatives_dir):
 
     df_features = []
     for subject in population:
+
+        missing_subs = []
         if not os.path.isfile(os.path.join(derivatives_dir, 'struct_cortical_thickness',
                                            '%s_ct2fsaverage5_fwhm20_lh.mgh' % subject)):
             print 'subject %s missing ct data' % subject
+            missing_subs.append(subject)
         else:
             ct_lh = nb.load(os.path.join(derivatives_dir, 'struct_cortical_thickness',
                                          '%s_ct2fsaverage5_fwhm20_lh.mgh' % subject)).get_data().ravel()
@@ -48,5 +51,5 @@ def return_ct_data(population, derivatives_dir):
                                  index=['ct_rh_' + str(i) for i in range(ct_rh.shape[0])])
         df_features.append(pd.concat([ct_lh, ct_rh], axis=0))
 
-    return pd.concat(df_features, axis=1)
+    return pd.concat(df_features, axis=1).drop(missing_subs, axis = 1)
 
