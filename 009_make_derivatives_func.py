4f01b1c7b60f962c5a96940c06637ef736e03fae
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-22 19:25:34 -0500

    push.

--- 009_make_derivatives_func.py
+++ 009_make_derivatives_func.py
@@ -82,16 +82,16 @@ def make_functional_derivatives(population, workspace_dir, freesurfer_dir, deriv
         print '1. Calculating Seed-Based Correlation'
 
         seeds = {'STR'         : mask_str,
-                 # 'STR3_MOTOR'  : mask_str_motor,
-                 # 'STR3_LIMBIC' : mask_str_limbic,
-                 # 'STR3_EXEC'   : mask_str_exec,
-                 # 'CAUD'        : mask_caud,
-                 # 'PUTA'        : mask_puta,
-                 # 'ACCU'        : mask_accu,
-                 # 'PALL'        : mask_pall,
-                 # 'THAL'        : mask_thal,
-                 # 'HIPP'        : mask_hipp,
-                 # 'AMYG'        : mask_amyg,
+                 'STR3_MOTOR'  : mask_str_motor,
+                 'STR3_LIMBIC' : mask_str_limbic,
+                 'STR3_EXEC'   : mask_str_exec,
+                 'CAUD'        : mask_caud,
+                 'PUTA'        : mask_puta,
+                 'ACCU'        : mask_accu,
+                 'PALL'        : mask_pall,
+                 'THAL'        : mask_thal,
+                 'HIPP'        : mask_hipp,
+                 'AMYG'        : mask_amyg,
                  }
 
         for seed_name in seeds:
