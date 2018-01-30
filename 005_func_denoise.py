10783a047771e09fb86b623ac2349b04ff55eca0
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-30 17:18:34 -0500

    push.

--- 005_func_denoise.py
+++ 005_func_denoise.py
@@ -56,7 +56,12 @@ def nuisance_signal_regression(population, workspace_dir):
         # id bad frames
         FD1D      = np.loadtxt(calculate_FD_Power(movpar))
         fd_frames_in = [frame for frame, val in enumerate(FD1D) if val < 0.2]
-        print 'Percentage of Good frames =', float((len(fd_frames_in)) / float(len(FD1D)))* 100
+        perc_good_frames = float((len(fd_frames_in)) / float(len(FD1D)))* 100
+
+        if perc_good_frames < 0.5:
+            print 'Percentage of Good frames is quite low... inspect subject and maybe throw out', perc_good_frames
+
+
         fd_frames_ex = set_frames_ex(in_file = FD1D, threshold=0.2, frames_before=1, frames_after=1)
 
 
