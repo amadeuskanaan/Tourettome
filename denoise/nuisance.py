f5fbc9e2a1ca800dbd2972037a0125bab90752ba
amadeuskanaan <amadeus.kanan@gmail.com>
2018-01-31 12:18:45 -0500

    push.

--- denoise/nuisance.py
+++ denoise/nuisance.py
@@ -330,7 +330,7 @@ def calc_residuals(subject,
 
     # insert the de-spiking regressor matrix here, if running de-spiking
     if frames_ex:
-        despike_mat = create_despike_regressor_matrix(frames_ex, nii.shape[3])
+        despike_mat = create_despike_regressor_matrix(frames_ex, data.shape[3])
         # this needs to be "is not None" instead of "if despike_mat:" because
         # despike_mat could be either a Numpy array or None
         if despike_mat is not None:
