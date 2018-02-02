46ce0c88dfbca0e90d08c5e26e56294dfe6afd2e
amadeuskanaan <amadeus.kanan@gmail.com>
2018-02-02 00:48:56 -0500

    push.

--- 009_make_derivatives_func.py
+++ 009_make_derivatives_func.py
@@ -211,6 +211,6 @@ def make_functional_derivatives(population, workspace_dir, freesurfer_dir, deriv
 
 # make_group_masks(tourettome_subjects, tourettome_workspace, tourettome_derivatives, FD_outliers)
 
-subs = [i for i in hannover_b+hamburg if i not in control_outliers+patient_outliers]
+subs = [i for i in paris if i not in control_outliers+patient_outliers]
 make_functional_derivatives(subs, tourettome_workspace, tourettome_freesurfer, tourettome_derivatives)
 
