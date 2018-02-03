f267796a0e9ef72fbab72898dab0ec93f9fea1cd
amadeuskanaan <amadeus.kanan@gmail.com>
2018-02-03 14:23:59 -0500

    push.

--- plotting/cmaps.py
+++ plotting/cmaps.py
@@ -15,3 +15,8 @@ second = (256-first)
 cmap_gradient = colors.LinearSegmentedColormap.from_list('my_colormap',
                                                   np.vstack((plt.cm.YlOrRd(np.linspace(0.98, 0.25, second)),
                                                             plt.cm.viridis(np.linspace(.98, 0.0, first)))))
+cmap_drysdale = colors.ListedColormap(['#00ffff', '#00afff','#0000ff', '#260000', '#530000','#fe0000', '#ff6a00',
+                                           '#ffff00'])
+
+cmap_ted = colors.ListedColormap(['#00ffff', '#00afff','#0000ff', '#260000', '#530000','#fe0000', '#ff6a00',
+                                      '#ffff00', '#ffffff'])
